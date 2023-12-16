07baa5d48dccb3858ec7257f034cc5cd305c8ad3
ice x <ice2600x@gmail.com>
2023-12-16 02:56:11 +0200

    Move handlers outside the schema

--- sharded_queue/__init__.py
+++ sharded_queue/__init__.py
@@ -1,8 +1,5 @@
-from sharded_queue.schema import (
-    Handler, Queue, Route, Tube, Worker, DeferredHandler, RecurrentHandler, DeferredRequest, RecurrentRequest
-)
+from sharded_queue.schema import Handler, Route, DeferredRequest, RecurrentRequest
+from sharded_queue.handlers import Queue, Tube, Worker, DeferredHandler, RecurrentHandler
 from sharded_queue.drivers import JsonTupleSerializer
 from sharded_queue.protocols import Lock, Serializer, Storage
 from sharded_queue.settings import WorkerSettings
-
-
