58bf88a1bef47b44a020f86e298d97be81a565d8
Ilya Chuvaev <ilya.chuvaev@basis.company>
2024-02-08 14:08:40 +0300

    recurent interval use total seconds

--- sharded_queue/__init__.py
+++ sharded_queue/__init__.py
@@ -371,7 +371,7 @@ class RecurrentRequest(NamedTuple):
     @classmethod
     def get_interval(cls, interval: int | float | timedelta) -> float:
         if isinstance(interval, timedelta):
-            return float(interval.seconds)
+            return float(interval.total_seconds())
 
         return float(interval)
 
